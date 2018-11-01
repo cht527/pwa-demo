a3dae3d8b4c3d4aa9c41bf13c2fa7fd65a98d243
caohaitao <caohaitao@58ganji.com>
2018-11-01 11:04:53 +0800

    update offline

--- src/views/offlinePage/index.vue
+++ src/views/offlinePage/index.vue
@@ -1,7 +1,9 @@
 
 <template>
-  <div>
+  <div class="offline-container">
     <h1 class="title">{{title}}</h1> 
+    <img class="offline-icon" src="../../assets/imgs/offline.png" alt="离线图标" />
+    <p><span>当前已处于离线状态！</span></p>
   </div>
 </template>
 
@@ -12,7 +14,7 @@ export default {
   
   data () {
     return {
-      title:"离线页面"
+      title:"offline"
     }
   },
 
@@ -24,5 +26,18 @@ export default {
 
 <style rel="stylesheet/less" lang="less">
 .title{text-align: center}
-
+ .offline-container{
+            height: 400px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center
+        }
+        .offline-container p{
+            font-size: 28px;
+        }
+        .offline-container p span{
+            font-size: 24px;
+            color:#f0610f
+        }
 </style>
